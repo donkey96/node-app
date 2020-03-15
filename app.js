37706160f965e2e425b1ffe3758e14e03b8c23ec
donkey96 <noire96@gmail.com>
2020-03-15 09:19:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const fs = require('fs');
+var request;
+var response;
 
 var server = http.createServer(getFromClient);
 
@@ -12,11 +14,12 @@ console.log('Server start!');
 function getFromClient(req,res){
   request = req;
   response = res;
-   fs.readFile('./index.html', 'UTF-8',
-    (error, data)=>{
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(data);
-      response.end();
-    }
-  );
-};
+   fs.readFile('./index.html', 'UTF-8', writeToResponse);
+}
+
+// readFile完了後の処理
+function writeToResponse(error, data){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(data);
+  response.end();
+}
