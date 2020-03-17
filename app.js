8e970fdba975078adc34a4a120cc763d09430d96
donkey96 <noire96@gmail.com>
2020-03-17 15:30:46 +0900

    ejs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
 
 var server = http.createServer(getFromClient);
 
@@ -10,15 +13,8 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(request,response){
-   fs.readFile('./index.html', 'UTF-8',
-    (error, data)=>{
-      var content = data.
-        replace(/dummy_title/g, 'タイトルです').
-        replace(/dummy_content/g, 'これがコンテンツです。');
-
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(content);
-      response.end();
-    }
-  );
-};
+  var content = ejs.render(index_page);
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(content);
+  response.end();
+}
