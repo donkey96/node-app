15ed3fdc82fe50c7c52d8eda084248a3057f1bd2
donkey96 <noire96@gmail.com>
2020-04-09 18:41:37 +0900

    response_indexを修正

--- app.js
+++ app.js
@@ -41,12 +41,7 @@ function getFromClient(request,response){
   }
 
   // 追加するデータ用の変数
-  var data = {
-    'Taro':'09-999-999',
-    'Hanako':'080-888-888',
-    'Sachiko':'070-777-777',
-    'Ichiro':'060-666-666'
-  };
+  var data = { msg:'no message...' };
 
   var data2 = {
     'Taro':['taro@yamada','09-999-999','Tokyo'],
@@ -57,18 +52,37 @@ function getFromClient(request,response){
 
   // ★indexのアクセス処理
   function response_index(request, response) {
-    var msg = "これはIndexページです。"
+    // POSTアクセス時の処理
+    if (request.method == 'POST') {
+      var body = '';
+
+      // データ受信のイベント処理
+      request.on('data', (data) => {
+        body += data;
+      });
+
+      // データ受信終了のイベント処理
+      request.on('end', () => {
+        data = qs.parse(body); // ★データのパース
+        write_index(request, response);
+      });
+    } else {
+      write_index(request, response);
+    }
+  }
+
+  // indexの表示の作成
+  function write_index(request, response) {
+    var msg = "※伝言を表示します。"
     var content = ejs.render(index_page, {
       title:"Index",
       content:msg,
       data:data,
-      filename:'data_item' // ★追記
     });
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(content);
     response.end();
   }
-
   // ★otherのアクセス処理
   function response_other(request, response) {
     var msg = "これはOtherページです。"
